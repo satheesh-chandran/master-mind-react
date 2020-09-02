778793ba4add1c795c7cea09408e1e44c1845f14
Satheesh C <satheesancchandran@gmail.com>
2020-09-02 14:27:14 +0530

    Updates

---
+++ precache-manifest.afb289a8f7760ee27d6e258e585e274d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d21aff9890ef6682ec754a48dea372a",
+    "url": "/master-mind-react/index.html"
+  },
+  {
+    "revision": "3bad5c5c7cebe9806dcd",
+    "url": "/master-mind-react/static/css/main.3ec227de.chunk.css"
+  },
+  {
+    "revision": "c7ce5607a37811cb0e52",
+    "url": "/master-mind-react/static/js/2.49b76165.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/master-mind-react/static/js/2.49b76165.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3bad5c5c7cebe9806dcd",
+    "url": "/master-mind-react/static/js/main.d2f83361.chunk.js"
+  },
+  {
+    "revision": "898b258ea3267fa40342",
+    "url": "/master-mind-react/static/js/runtime-main.3e2d9dae.js"
+  }
+]);
